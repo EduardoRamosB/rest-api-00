e8cc7cb088bc511d6de70900a207c7d8e95a27d9
EduardoR <eduardo.ramos.b1@gmail.com>
2024-09-04 16:59:19 -0500

    Add created_at and updated_at

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,14 @@
+from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, db_index=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.email

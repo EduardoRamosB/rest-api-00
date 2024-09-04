d470853573c3490814b0e132f2019757f4207cbd
EduardoR <eduardo.ramos.b1@gmail.com>
2024-09-04 16:39:33 -0500

    Add accounts app

--- backend02/settings.py
+++ backend02/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend02_dev',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 

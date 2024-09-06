29dde8d675c70cc5d6228e40df1ab721ed2ffe59
EduardoR <eduardo.ramos.b1@gmail.com>
2024-09-06 00:05:12 -0500

    Add Animals create, read  and update

--- shelter/serializers.py
+++ shelter/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 
+from accounts.models import CustomUser
 from shelter.models import Animal, Adoption
 
 
 class AnimalSerializer(serializers.ModelSerializer):
+    created_by_id = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), source='created_by',
+                                                       write_only=True, required=False)
+    updated_by_id = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), source='updated_by',
+                                                       write_only=True)
+
+    created_by = serializers.ReadOnlyField(source='created_by.email')
+    updated_by = serializers.ReadOnlyField(source='updated_by.email')
+
     class Meta:
         model = Animal
-        fields = ['id', 'name', 'age', 'breed', 'kind', 'status', 'reason', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'age', 'breed', 'kind', 'status', 'reason', 'created_at', 'updated_at', 'created_by_id',
+                  'updated_by_id', 'created_by', 'updated_by']
+
+    def update(self, instance, validated_data):
+        validated_data.pop('created_by', None)
+        return super().update(instance, validated_data)
 
 
 class AdoptionSerializer(serializers.ModelSerializer):
--- shelter/views.py
+++ shelter/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 
+from accounts.models import CustomUser
 from shelter.models import Animal, Adoption
 from shelter.serializers import AnimalSerializer, AdoptionSerializer
 
@@ -8,6 +10,20 @@ from shelter.serializers import AnimalSerializer, AdoptionSerializer
 class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+
+    def perform_create(self, serializer):
+        created_by_id = self.request.data.get('created_by_id')
+        updated_by_id = self.request.data.get('updated_by_id')
+
+        if not created_by_id:
+            raise ValidationError("Created By ID is required.")
+        if not updated_by_id:
+            raise ValidationError("Updated By ID is required.")
+
+        created_by = get_object_or_404(CustomUser, id=created_by_id)
+        updated_by = get_object_or_404(CustomUser, id=updated_by_id)
+
+        serializer.save(created_by=created_by, updated_by=updated_by)
 
 
 class AdoptionViewSet(viewsets.ModelViewSet):
